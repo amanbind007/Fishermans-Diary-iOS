7444f2f744ac28898c1408a260f9dcf182bd14c9
Aman <amanbind007@gmail.com>
2023-10-06 21:27:06 +0530

    Initial Commit

---
+++ Fisherman's Keeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fisherman's Keeper
+//
+//  Created by Aman Bind on 06/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fisherman's Keeper/Fisherman_s_KeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fisherman_s_KeeperApp.swift
+//  Fisherman's Keeper
+//
+//  Created by Aman Bind on 06/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Fisherman_s_KeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
