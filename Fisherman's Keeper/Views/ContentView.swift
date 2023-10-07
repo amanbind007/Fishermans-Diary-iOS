499af9a4ad6969a7a3ffc16f9c3db84c710cffc9
Aman <amanbind007@gmail.com>
2023-10-07 23:00:49 +0530

    Added API Response JSON t o makee it offline

--- Fisherman's Keeper/Views/ContentView.swift
+++ Fisherman's Keeper/Views/ContentView.swift
@@ -9,21 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var searchText : String  = ""
+    
     var body: some View {
         NavigationStack{
-            VStack{
-                ScrollView {
-                    Grid{
-                        HStack{
-                            FishCardView()
-                            FishCardView()
+            
+            TabView{
+                
+                VStack{
+                    ScrollView {
+                        Grid{
+                            HStack{
+                                FishCardView()
+                                FishCardView()
+                            }
+                            
                         }
                         
                     }
-                    
                 }
+                
+                
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                    Text("Search Fish")
+                }
+                
+                MyFishListView()
+                    .tabItem {
+                        Image(systemName: "fish")
+                        Text("My Fish List")
+                        
+                    }
+                
+                
+                    
+                
+                
             }
-            
+            .searchable(text: $searchText)
+            .navigationTitle("Search Fish")
         }
     }
 }
---
+++ Fisherman's Keeper/Views/MyFishListView.swift
@@ -0,0 +1,18 @@
+//
+//  MyFishListView.swift
+//  Fisherman's Keeper
+//
+//  Created by Aman Bind on 07/10/23.
+//
+
+import SwiftUI
+
+struct MyFishListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyFishListView()
+}
