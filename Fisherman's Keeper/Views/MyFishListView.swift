4eaf0c74ce9009c8b7c17cf20fc4e8a2161a52b0
Aman <amanbind007@gmail.com>
2023-10-31 22:44:13 +0530

    added deletion for my fish list view using swipe gesture

--- Fisherman's Keeper/Views/MyFishListView.swift
+++ Fisherman's Keeper/Views/MyFishListView.swift
@@ -9,19 +9,33 @@ import SwiftData
 import SwiftUI
 
 struct MyFishListView: View {
+    @Environment(\.modelContext) var context
+
     @Query var fishData: [FishData]
 
     @State var searchText: String?
+    @State private var selectedFish: FishData?
+
     var body: some View {
         NavigationStack {
-            List {
+            List(selection: $selectedFish) {
                 ForEach(fishData, id: \.scientificName) { fish in
 
                     MyFishListItemView(fishData: fish)
+                        .swipeActions(edge: .trailing) {
+                            Button(role: .destructive) {
+                                context.delete(fish)
+                                //selectedThing = nil
+                                do {
+                                    try context.save()
+                                } catch {
+                                    print(error.localizedDescription)
+                                }
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                        }
                 }
-                .onDelete(perform: { _ in
-                    // perform
-                })
             }
             .searchable(text: $searchText ?? "")
             .navigationTitle("My Fish List")
